ea4dbafd33902a604a7cde4a440eca1683b309f2
khanxu <khanxu@global.tencent.com>
2023-09-26 15:26:38 -0700

    add test_solve_comp

--- tests/test_multivariable.py
+++ tests/test_multivariable.py
@@ -20,85 +20,91 @@ class FacePatch:
         t = np.dot(alphas, self._deltas)
         return self._neutral + t
 
-    def calc_partial_derv(self, vertex_index, alphas, alpha_index, v_target):
+    def calc_f_partial_derv_comp(self, vertex_index, alphas, alpha_index, v_target):
         vertex_deltas = self._deltas[vertex_index]
-        f = v_target - (self._neutral[vertex_index] + np.dot(alphas, vertex_deltas))
-        t3 = np.dot(f, vertex_deltas[alpha_index])
-        return -2 * t3
+        d = vertex_deltas[:, alpha_index]
+        return -d
 
-    def calc_partial_derv_comp(self, vertex_index, alphas, alpha_index, v_target):
-        vertex_deltas = self._deltas[vertex_index]
-        f = v_target - (self._neutral[vertex_index] + np.dot(alphas, vertex_deltas))
-        t3 = np.multiply(f, vertex_deltas[alpha_index])
-        return -2 * t3
-
-    def calc_jacobian(self, in_alphas, v_targets):
-        num_vertices = self.get_num_vertices()
-        assert (v_targets.shape[0] == num_vertices)
-        num_delta_shapes = self.get_num_delta_shapes()
-
-        jacobian = np.zeros((num_vertices, num_delta_shapes))
-        for vertex_index in range(num_vertices):
-            for alpha_index in range(num_delta_shapes):
-                v_target = v_targets[vertex_index]
-                jacobian[vertex_index, alpha_index] = self.calc_partial_derv(vertex_index, in_alphas, alpha_index, v_target)
-
-        return jacobian
-
-    def calc_jacobian_comp(self, in_alphas, v_targets):
+    def calc_f_jacobian_comp(self, in_alphas, v_targets):
         num_vertices = self.get_num_vertices()
         assert (v_targets.shape[0] == num_vertices)
         num_delta_shapes = self.get_num_delta_shapes()
 
         jacobian = np.zeros((num_vertices * 3, num_delta_shapes))  # 3d vertex
         for vertex_index in range(num_vertices):
+            v_target = v_targets[vertex_index]
             for alpha_index in range(num_delta_shapes):
-                v_target = v_targets[vertex_index]
-                derv_comp = self.calc_partial_derv_comp(vertex_index, in_alphas, alpha_index, v_target)
+                derv_comp = self.calc_f_partial_derv_comp(vertex_index, in_alphas, alpha_index, v_target)
                 fill_index = vertex_index * 3
                 jacobian[fill_index:fill_index + 3, alpha_index] = derv_comp
 
         return jacobian
 
-    def test_solve_comp(self, in_alphas, v_targets):
+    def test_solve_comp(self, in_alphas, v_targets, in_max_iter):
         alphas = np.copy(in_alphas)
         print(alphas)
 
-        for i in range(10):
-            jacobian = self.calc_jacobian_comp(alphas, v_targets)
-            A = np.matmul(jacobian.transpose(), jacobian)
-            # print(A)
+        for i in range(in_max_iter):
+            jacobian = self.calc_f_jacobian_comp(alphas, v_targets)
+
+            # validation:
+            for vertex_index in range(self.get_num_vertices()):
+                v_target = v_targets[vertex_index]
+                for alpha_index in range(self.get_num_delta_shapes()):
+                    d_c0 = calc_partial_derv_numerical_cwise(self, alphas, alpha_index, vertex_index, v_target, 0)
+                    d_c1 = calc_partial_derv_numerical_cwise(self, alphas, alpha_index, vertex_index, v_target, 1)
+                    d_c2 = calc_partial_derv_numerical_cwise(self, alphas, alpha_index, vertex_index, v_target, 2)
+                    assert (abs(jacobian[vertex_index * 3 + 0, alpha_index] - d_c0) < 0.0001)
+                    assert (abs(jacobian[vertex_index * 3 + 1, alpha_index] - d_c1) < 0.0001)
+                    assert (abs(jacobian[vertex_index * 3 + 2, alpha_index] - d_c2) < 0.0001)
+
             shape = self.forward_pass(alphas)
             r = v_targets - shape
             r_flatten = r.flatten()
+            # r_norm = np.linalg.norm(r_flatten)
+            # print(r_norm)
+
+            A = np.matmul(jacobian.transpose(), jacobian)
+            # print(A)
             b = -np.matmul(jacobian.transpose(), r_flatten)
             # print(b)
+            gh1 = scipy.linalg.solve(A, b)
+            # print(gh1)
+            alphas += gh1
+            if np.linalg.norm(gh1) < 0.00001:
+                break
 
-            gh = scipy.linalg.solve(A, b)
-            # print(gh)
-            alphas += gh
+            # matrix inverse is slow
+            # gh3 = np.linalg.inv(jacobian.T @ jacobian) @ jacobian.T @ r_flatten
+            # # print(gh3)
+            # alphas -= gh3
+            # if np.linalg.norm(gh3) < 0.00000001:
+            #     break
 
         print(alphas)
         print('done')
         return alphas
 
 
-def calc_partial_derv_numerical(in_model, in_alphas, alpha_index, vertex_index, v_target):
+# component_wise version:
+def calc_partial_derv_numerical_cwise(in_model, in_alphas, alpha_index, vertex_index, v_target, comp_index):
     eps = 0.0001
     perb = np.zeros(in_alphas.shape)
     perb[alpha_index] = eps
     f1 = v_target - in_model.forward_pass(in_alphas - perb)[vertex_index]
     f2 = v_target - in_model.forward_pass(in_alphas + perb)[vertex_index]
-    y1 = np.dot(f1, f1)
-    y2 = np.dot(f2, f2)
-    derv = (y2 - y1) / (2 * eps)
+    derv = (f2[comp_index] - f1[comp_index]) / (2 * eps)
+    # y1 = np.dot(f1[comp_index], f1[comp_index])
+    # y2 = np.dot(f2[comp_index], f2[comp_index])
+    # derv = (y2 - y1) / (2 * eps)
     return derv
 
 
-def test_main():
+def test_main_3():
     patch_neutral = np.array([[1, 1, 1], [2, 2, 2], [3, 3, 3], [4, 4, 4]])
     patches_deltas = np.array(
-        [[[1, 0, 0], [0, 0.5, 0], [0, 0, 2]],  # deltas for the first vertex
+        [# [[1, 0, 0], [0, 0.5, 0], [0, 0, 2]],  # deltas for the first vertex
+         [[1, 0, 0], [0, 1, 0], [0, 0, 1]],
          [[1, 0, 0], [0, 1, 0], [0, 0, 1]],  # deltas for the second vertex
          [[0.5, 0, 0], [0, 0.5, 0], [0, 0, 0.5]],  # deltas for the third vertex
          [[0.3, 0, 0], [0, 0.3, 0], [0, 0, 0.3]]],
@@ -112,24 +118,24 @@ def test_main():
 
     v_targets = np.array([[1.3, 1.4, 1.5], [2.3, 2.4, 2.5], [3.1, 3.2, 3.3], [4.3, 4.2, 4.1]])
 
-    for alpha_index in range(3):
+    print("valid1:")
+    for vertex_index in range(4):
+        v_target = v_targets[vertex_index]
         str = ''
-        for vertex_index in range(4):
-            d1 = face_model.calc_partial_derv(vertex_index, alphas1, alpha_index, v_targets[vertex_index])
-            d2 = calc_partial_derv_numerical(face_model, alphas1, alpha_index, vertex_index, v_targets[vertex_index])
-            str += f' [{d2}] '
+        for alpha_index in range(3):
+            d2_c0 = calc_partial_derv_numerical_cwise(face_model, alphas1, alpha_index, vertex_index, v_target, 0)
+            d2_c1 = calc_partial_derv_numerical_cwise(face_model, alphas1, alpha_index, vertex_index, v_target, 1)
+            d2_c2 = calc_partial_derv_numerical_cwise(face_model, alphas1, alpha_index, vertex_index, v_target, 2)
+            str += f' [{d2_c0, d2_c1, d2_c2}] '
         print(str)
 
-    jacobian = face_model.calc_jacobian(alphas1, v_targets)
-    print(jacobian)
-
-    jacobian_comp = face_model.calc_jacobian_comp(alphas1, v_targets)
+    jacobian_comp = face_model.calc_f_jacobian_comp(alphas1, v_targets)
     print(jacobian_comp)
 
-    alphas2 = face_model.test_solve_comp(alphas1, v_targets)
+    alphas2 = face_model.test_solve_comp(alphas1, v_targets, 100)
     p = face_model.forward_pass(alphas2)
     print(p)
 
 
 if __name__ == '__main__':
-    test_main()
+    test_main_3()
