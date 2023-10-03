e73916c05e348a0d94e93edb532446baefe0ae4c
khanxu <khanxu@global.tencent.com>
2023-10-02 20:40:56 -0700

    test done.

--- tests/test_facepatch_fitting.py
+++ tests/test_facepatch_fitting.py
@@ -19,17 +19,23 @@ def test_misc(face_model, alphas1, v_targets):
     print(jacobian_comp)
 
 
+def calc_r_norm(face_model, alphas, w_sparse, v_targets):
+    v = face_model.forward_pass(alphas)
+    r1 = v_targets - v
+    r1_flatten = r1.flatten()
+    r2 = alphas * w_sparse
+    r_flatten = np.concatenate((r1_flatten, r2), axis=0)
+    return np.linalg.norm(r_flatten)
+
+
 def test_solve1(face_model, alphas1, v_targets):
+    print('test_solve1:')
     alphas_best = face_model.solve_alphas_cwise(alphas1, v_targets, 100)
     print(f'alphas_best:{alphas_best}')
-    v_best = face_model.forward_pass(alphas_best)
-    print(v_best)
-    r_best = v_targets - v_best
-    r_best_norm = np.linalg.norm(r_best)
-    print(r_best)
-    print(r_best_norm)
+    r_best_norm = calc_r_norm(face_model, alphas_best, 0.0, v_targets)
+    print(f'r_best_norm:{r_best_norm}')
 
-    eps = 0.00001
+    eps = 0.0001
     alphas_perbs = []
     alphas_perbs.append(alphas_best - np.array([eps, 0, 0]))
     alphas_perbs.append(alphas_best + np.array([eps, 0, 0]))
@@ -39,18 +45,32 @@ def test_solve1(face_model, alphas1, v_targets):
     alphas_perbs.append(alphas_best + np.array([0, 0, eps]))
 
     for i in range(len(alphas_perbs)):
-        v = face_model.forward_pass(alphas_perbs[i])
-        r = v_targets - v
-        r_norm = np.linalg.norm(r)
-        print(r_norm)
+        r_norm = calc_r_norm(face_model, alphas_perbs[i], 0.0, v_targets)
+        print(r_norm - r_best_norm)
         assert(r_norm > r_best_norm)
 
 
-def test_solve2(face_model, alphas1, v_targets):
-    alphas_best_2 = face_model.solve_alphas_cwise_2(alphas1, 1.0, v_targets, 100)
-    print(f'alphas_best_2:{alphas_best_2}')
-    v_best_2 = face_model.forward_pass(alphas_best_2)
-    print(v_best_2)
+def test_solve2(face_model, alphas, v_targets):
+    print('test_solve2:')
+    w_sparse = 5.0
+    alphas_best = face_model.solve_alphas_cwise_2(alphas, w_sparse, v_targets, 100)
+    print(f'alphas_best:{alphas_best}')
+    r_best_norm = calc_r_norm(face_model, alphas_best, w_sparse, v_targets)
+    print(f'r_best_norm:{r_best_norm}')
+
+    eps = 0.0001
+    alphas_perbs = []
+    alphas_perbs.append(alphas_best - np.array([eps, 0, 0]))
+    alphas_perbs.append(alphas_best + np.array([eps, 0, 0]))
+    alphas_perbs.append(alphas_best - np.array([0, eps, 0]))
+    alphas_perbs.append(alphas_best + np.array([0, eps, 0]))
+    alphas_perbs.append(alphas_best - np.array([0, 0, eps]))
+    alphas_perbs.append(alphas_best + np.array([0, 0, eps]))
+
+    for i in range(len(alphas_perbs)):
+        r_norm = calc_r_norm(face_model, alphas_perbs[i], w_sparse, v_targets)
+        print(r_norm - r_best_norm)
+        assert(r_norm > r_best_norm)
 
 
 def test_main_3():
@@ -71,7 +91,7 @@ def test_main_3():
 
     v_targets = np.array([[1.3, 1.4, 1.5], [2.3, 2.4, 2.5], [3.1, 3.2, 3.3], [4.3, 4.2, 4.1]])
 
-    test_misc(face_model, alphas1, v_targets)
+    # test_misc(face_model, alphas1, v_targets)
     test_solve1(face_model, alphas1, v_targets)
     test_solve2(face_model, alphas1, v_targets)
 
